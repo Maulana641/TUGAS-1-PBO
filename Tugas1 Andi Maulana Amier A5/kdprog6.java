1d5abd73fca43c5e248be7685e68bea11a14339d
Maulana641 <62836628+Maulana641@users.noreply.github.com>
2021-03-16 20:37:24 +0700

    Add files via upload

---
+++ Tugas1 Andi Maulana Amier A5/kdprog6.java
@@ -0,0 +1,19 @@
+public class kdprog6 {
+    public static void main(String[]args){
+		int a=5,b=6;
+		float c=7.f,d=8.3f;
+		char e='F';
+		double g=3.14;
+		
+		System.out.println((float)a); //int ke float//
+		System.out.println((double)b);//int ke double//
+		System.out.println((int)c); //float ke int//
+		System.out.println((double)d); //float ke double//
+		System.out.println((int)e); //char ke int//
+		System.out.println((float)e);//char ke float//
+		System.out.println((double)e);//char ke double//
+		System.out.println((int)g); //double ke int//
+		System.out.println((float)g); //double ke float//
+	}
+}	
+
